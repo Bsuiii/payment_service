d38edb1d436b6478516490583955a5035eeacf8d
Haitam Bessi <Haitambessi@gmail.com>
2025-07-16 14:48:34 +0100

    application prod file

--- src/main/java/com/example/payment_service/entity/Paiement.java
+++ src/main/java/com/example/payment_service/entity/Paiement.java
@@ -1,15 +1,14 @@
 package com.example.payment_service.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "paiement")
